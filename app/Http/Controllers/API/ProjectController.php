615cb4a41cbaf48cab84f445df73f48ebad3919f
Francesco-Iannuzzi <iannuzzi.francesco@outlook.com>
2023-06-15 16:09:33 +0200

    paginate 8

--- app/Http/Controllers/API/ProjectController.php
+++ app/Http/Controllers/API/ProjectController.php
@@ -8,23 +8,23 @@ use App\Models\Project;
 
 class ProjectController extends Controller
 {
-    public function index() {
-        $projects = Project::with('type', 'technologies', 'user')->orderByDesc('id')->paginate(4);
+    public function index()
+    {
+        $projects = Project::with('type', 'technologies', 'user')->orderByDesc('id')->paginate(8);
 
         return response()->json([
             'success' => true,
             'result' => $projects,
         ]);
-        
     }
 
-    public function show($slug) {
+    public function show($slug)
+    {
         $project = Project::with('type', 'technologies', 'user')->where('slug', $slug)->first();
 
         return response()->json([
             'success' => true,
             'result' => $project,
         ]);
-        
     }
 }
