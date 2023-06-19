825485055877d1136acf332d04da889080989867
Francesco-Iannuzzi <iannuzzi.francesco@outlook.com>
2023-06-19 19:02:57 +0200

    first step style mail

--- resources/views/emails/new_contact.blade.php
+++ resources/views/emails/new_contact.blade.php
@@ -2,15 +2,15 @@
     # Introduction
 
 
-    <strong>You have received a new message from a user in your portfolio.</strong>
-    <strong>Name: </strong> {{ $lead->name }}
-    <strong>Email: </strong> {{ $lead->email }}
-    <strong>Message: </strong><br>
+    You have received a new message from a user in your portfolio.
+    Name: {{ $lead->name }}
+    Email: {{ $lead->email }}
+    Message: <br>
     <x-mail::panel>
         {{ $lead->message }}
     </x-mail::panel>
 
-    <x-mail::button :url="''">
+    <x-mail::button url="#">
         Button Text
     </x-mail::button>
 
