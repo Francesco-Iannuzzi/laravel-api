ed7488f0da24780e93b3758394df26ece12e231b
Francesco-Iannuzzi <iannuzzi.francesco@outlook.com>
2023-06-20 20:15:15 +0200

    add db.php projects

--- config/db.php
+++ config/db.php
@@ -3,14 +3,202 @@
 return [
     'projects' => [
         [
-            'title' => '',
-            'made_by' => '',
-            'description' => '',
-            'date' => '',
+            'title' => 'Discord',
+            'made_by' => 'Francesco Iannuzzi',
+            'description' => 'Replica of the Discord website layout.',
+            'date' => '2023-02-07',
             'cover' => '',
             'link' => '',
-            'code_link' => '',
-            'trace' => '',
+            'code_link' => 'https://github.com/Francesco-Iannuzzi/htmlcss-discord',
+            'trace' => 'Replica of the Discord website layout.',
+        ],
+        [
+            'title' => 'Boolando',
+            'made_by' => 'Francesco Iannuzzi',
+            'description' => 'Replica of the Zoolando website layout.',
+            'date' => '2023-02-08',
+            'cover' => '',
+            'link' => '',
+            'code_link' => 'https://github.com/Francesco-Iannuzzi/html-css-boolando',
+            'trace' => 'Replica of the Zoolando website layout, using position.',
+        ],
+        [
+            'title' => 'Dropbox',
+            'made_by' => 'Francesco Iannuzzi',
+            'description' => 'Replica of the Dropbox website layout.',
+            'date' => '2023-02-16',
+            'cover' => '',
+            'link' => '',
+            'code_link' => 'https://github.com/Francesco-Iannuzzi/htmlcss-dropbox/tree/main',
+            'trace' => 'Replica of the Dropbox website layout, using flex',
+        ],
+        [
+            'title' => 'Responsive Layout',
+            'made_by' => 'Francesco Iannuzzi',
+            'description' => 'Creation of a responsive layout.',
+            'date' => '2023-02-21',
+            'cover' => '',
+            'link' => '',
+            'code_link' => 'https://github.com/Francesco-Iannuzzi/htmlcss-responsive-layout/tree/main',
+            'trace' => 'Create a responsive layout, using media query',
+        ],
+        [
+            'title' => 'Bootstrap Freelancer',
+            'made_by' => 'Francesco Iannuzzi',
+            'description' => 'Creation of a stylized layout with Bootstrap.',
+            'date' => '2023-02-28',
+            'cover' => '',
+            'link' => '',
+            'code_link' => 'https://github.com/Francesco-Iannuzzi/html-css-bootstrap-freelancer',
+            'trace' => 'Create a stylized layout with Bootstrap.',
+        ],
+        [
+            'title' => 'DashBoard',
+            'made_by' => 'Francesco Iannuzzi',
+            'description' => 'Management Dashboard',
+            'date' => '2023-03-03',
+            'cover' => '',
+            'link' => '',
+            'code_link' => 'https://github.com/Francesco-Iannuzzi/html-css-bootstrap-dashboard',
+            'trace' => 'Create a Dashboard using Bootstrap and Media Query',
+        ],
+        [
+            'title' => 'Train Ticket Generator',
+            'made_by' => 'Francesco Iannuzzi',
+            'description' => 'A program that asks the user for their age and the distance to be traveled, and based on the received data, creates different tickets for age groups.',
+            'date' => '2023-03-10',
+            'cover' => '',
+            'link' => '',
+            'code_link' => 'https://github.com/Francesco-Iannuzzi/js-biglietto-treno-form',
+            'trace' => 'Write a program in Javascript that asks the user for:
+                1) The number of kilometers to be traveled
+                2) Passenger\'s age.
+                Based on this information, it should calculate the total price of the travel ticket according to the following rules:
+                    - The ticket price is defined based on the kilometers (€0.21 per km)
+                    - A 20% discount should be applied for minors
+                    - A 40% discount should be applied for passengers over 65.',
+        ],
+        [
+            'title' => 'FizzBuzz',
+            'made_by' => 'Francesco Iannuzzi',
+            'description' => 'The Famous FIZZBUZZ!',
+            'date' => '2023-03-13',
+            'cover' => '',
+            'link' => '',
+            'code_link' => 'https://github.com/Francesco-Iannuzzi/js-fizzbuzz',
+            'trace' => 'Write a program that prints numbers from 1 to 100, but:
+            - for multiples of 3, print "Fizz" instead of the number
+            - for multiples of 5, print "Buzz"
+            - for numbers that are multiples of both 3 and 5, print "FizzBuzz"',
+        ],
+        [
+            'title' => 'Minesweeper',
+            'made_by' => 'Francesco Iannuzzi',
+            'description' => 'Grid-based game clearing squares while avoiding hidden mines strategically.',
+            'date' => '2023-03-23',
+            'cover' => '',
+            'link' => '',
+            'code_link' => 'https://github.com/Francesco-Iannuzzi/js-campominato-dom',
+            'trace' => 'Replicate the game Minesweeper.
+            Add a dropdown menu next to the generation button, providing a choice between three different difficulty levels:
+            - Difficulty 1 ⇒ 100 cells, with a number ranging from 1 to 100, divided into 10 cells per 10 rows.
+            - Difficulty 2 ⇒ 81 cells, with a number ranging from 1 to 81, divided into 9 cells per 9 rows.
+            - Difficulty 3 ⇒ 49 cells, with a number ranging from 1 to 49, divided into 7 cells per 7 rows.',
+        ],
+        [
+            'title' => 'Social Posts',
+            'made_by' => 'Francesco Iannuzzi',
+            'description' => 'Replica of a Social.',
+            'date' => '2023-03-24',
+            'cover' => '',
+            'link' => '',
+            'code_link' => 'https://github.com/Francesco-Iannuzzi/js-social-posts',
+            'trace' => 'Recreate a social feed by adding a JavaScript script where each post should have the necessary information to display its corresponding card:
+            - Post ID: a progressive number from 1 to n.
+            - Author\'s name.
+            - Author\'s photo.
+            - Date.
+            - Post text.
+            - Image (not all posts need to have an image).
+            Number of likes. If we click the \'Like\' button, change the text color of the button and increment the corresponding like counter.',
+        ],
+        [
+            'title' => 'Vue Slider',
+            'made_by' => 'Francesco Iannuzzi',
+            'description' => 'Carousel with VUE',
+            'date' => '2023-03-30',
+            'cover' => '',
+            'link' => '',
+            'code_link' => 'https://github.com/Francesco-Iannuzzi/vue-slider',
+            'trace' => 'Create a Carousel with Vue',
+        ],
+        [
+            'title' => 'BoolZapp',
+            'made_by' => 'Francesco Iannuzzi',
+            'description' => 'Replica of the WhatsApp App.',
+            'date' => '2023-04-11',
+            'cover' => '',
+            'link' => '',
+            'code_link' => 'https://github.com/Francesco-Iannuzzi/vue-boolzapp',
+            'trace' => 'Replica of the design with the ability to have user-written messages (in green) and interlocutor-written messages (in white) by assigning two different CSS classes.
+            - Dynamic display of the contact list: using the v-for directive, display the name and image of each contact.
+            - Dynamic display of messages: using the v-for directive, display all messages related to the active contact within the conversation panel.
+            - Clicking on a contact shows the conversation of the clicked contact.
+            - Adding a message: the user types a text in the bottom part and pressing \'enter\' adds the text to the thread above as a green message.
+            - Interlocutor\'s response: upon entering a message, the user will receive an \'ok\' response, which appears after 1 second.
+            - User search: by typing something in the input on the left, only contacts whose names contain the entered letters are displayed (e.g., Marco, Matteo, Martina -> I type \'mar\' and only Marco and Martina remain).
+            - Delete message: clicking on a message displays a dropdown menu that allows deleting the selected message.
+            - Display of time and last sent/received message in the contact list.',
+        ],
+        [
+            'title' => 'Vite Yu-Gi-Oh',
+            'made_by' => 'Francesco Iannuzzi',
+            'description' => 'Database of Yu-Gi-OH cards',
+            'date' => '2023-04-18',
+            'cover' => '',
+            'link' => '',
+            'code_link' => 'https://github.com/Francesco-Iannuzzi/vite-yu-gi-oh',
+            'trace' => 'Use Api and Axios to create Yu-Gi-Oh cards Database',
+        ],
+        [
+            'title' => 'Boolflix',
+            'made_by' => 'Francesco Iannuzzi',
+            'description' => 'Replica of Netflix',
+            'date' => '2023-04-21',
+            'cover' => '',
+            'link' => '',
+            'code_link' => 'https://github.com/Francesco-Iannuzzi/vite-boolflix',
+            'trace' => 'Replicate Netflix Movies',
+        ],
+        [
+            'title' => 'Project HTML VUE JS',
+            'made_by' => 'Francesco Iannuzzi',
+            'description' => 'Site with Vue, JS and SCSS using components.',
+            'date' => '2023-04-28',
+            'cover' => '',
+            'link' => '',
+            'code_link' => 'https://github.com/Francesco-Iannuzzi/proj-html-vuejs/tree/master',
+            'trace' => 'Create a Project with HTML, VUE and JS.',
+        ],
+        [
+            'title' => 'PHPassword Generator',
+            'made_by' => 'Francesco Iannuzzi',
+            'description' => 'Password generator with PHP',
+            'date' => '2023-05-08',
+            'cover' => '',
+            'link' => '',
+            'code_link' => 'https://github.com/Francesco-Iannuzzi/php-strong-password-generator',
+            'trace' => 'We need to create a page that allows our users to use our (fairly) secure password generator.',
+        ],
+        [
+            'title' => 'DC Comics',
+            'made_by' => 'Francesco Iannuzzi',
+            'description' => 'Replica of the DC Comics website.',
+            'date' => '2023-06-01',
+            'cover' => '',
+            'link' => '',
+            'code_link' => 'https://github.com/Francesco-Iannuzzi/laravel-dc-comics',
+            'trace' => 'Replicate the DC Comics website in Laravel.',
         ],
     ]
 ];
