7570b91d0803d33965a3d17145df9e4041fa4dc4
Francesco-Iannuzzi <iannuzzi.francesco@outlook.com>
2023-06-22 15:00:40 +0200

    change mail riceived (new_contact.blade)

--- resources/views/emails/new_contact.blade.php
+++ resources/views/emails/new_contact.blade.php
@@ -1,16 +1,12 @@
-<x-mail::message>
+@component('vendor.mail.html.message')
     # Introduction
 
 
-    You have received a new message from a user in your portfolio.
+    You have received a new message from an user in your portfolio.
     Name: {{ $lead->name }}
     Email: {{ $lead->email }}
     Message: {{ $lead->message }}
 
-    <x-mail::button url="''">
-        Button Text
-    </x-mail::button>
-
     Thanks,
     {{ config('app.name') }}
-</x-mail::message>
+@endcomponent
